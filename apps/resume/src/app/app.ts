397f6aff044f0e1d4fbc0d4bfffff32e66c55999
Leith Osborne <osborneleith@gmail.com>
2025-07-02 21:22:50 +0100

    cleanup

--- apps/resume/src/app/app.ts
+++ apps/resume/src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
