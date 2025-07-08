5409270418265aeee8a91a06960119936bb0b39d
Leith Osborne <osborneleith@gmail.com>
2025-07-08 22:59:17 +0100

    themes

--- apps/resume/src/app/app.ts
+++ apps/resume/src/app/app.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { Paper } from './paper/paper';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,42 @@ import { Paper } from './paper/paper';
   styleUrl: './app.scss',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [Paper],
+  imports: [Paper, CommonModule],
 })
 export class App {
   protected title = 'resume';
   dataTheme = signal<string>('corporate');
+
+  themes = [
+    'light',
+    'dark',
+    'cupcake',
+    'bumblebee',
+    'emerald',
+    'corporate',
+    'synthwave',
+    'retro',
+    'cyberpunk',
+    'valentine',
+    'halloween',
+    'garden',
+    'forest',
+    'aqua',
+    'pastel',
+    'fantasy',
+    'wireframe',
+    'dracula',
+    'cmyk',
+    'autumn',
+    'business',
+    'acid',
+    'lemonade',
+    'night',
+    'winter',
+    'dim',
+    'nord',
+    'sunset',
+  ];
   setTheme(theme: string) {
     this.dataTheme.set(theme);
   }
--- apps/resume/tailwind.config.js
+++ apps/resume/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require('tailwind-scrollbar')],
   daisyui: {
     themes: [
       'light',
