f67ccdb7a70b9169524f75d8b746165ef98f532d
Leith Osborne <osborneleith@gmail.com>
2025-09-14 11:34:04 +0100

    rm eight-puzzle

--- apps/eight-puzzle/src/app/directives/drag-and-drop-demo-images.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DragAndDropDemoImagesDirective } from './drag-and-drop-demo-images.directive';
-
-describe('DragAndDropDemoImagesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DragAndDropDemoImagesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- apps/eight-puzzle/src/app/directives/draggable-grid-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DraggableGridItemDirective } from './draggable-grid-item.directive';
-
-describe('DraggableGridItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DraggableGridItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
