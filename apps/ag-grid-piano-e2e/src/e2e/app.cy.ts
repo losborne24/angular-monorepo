cdb757f9ce6b099c37308a3cdc561c9d7d558b15
Leith Osborne <osborneleith@gmail.com>
2025-12-16 20:35:16 +0000

    remove

--- apps/ag-grid-piano-e2e/src/e2e/app.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('ag-grid-piano-e2e', () => {
-  beforeEach(() => cy.visit('/'));
-
-  describe('Piano Grid Display', () => {
-    it('should display the AG Grid piano component', () => {
-      cy.get('ag-grid-angular').should('be.visible');
-    });
-  });
-});
